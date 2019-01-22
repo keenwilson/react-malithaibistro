2fc7ee5ba1aee2fdc310dde847e8bb3350fa2451
Keen Wilson <40321520+keenwilson@users.noreply.github.com>
2019-01-22 12:05:39 -0600

    Add From the wok

--- src/components/Menu/fromTheWok.js
+++ src/components/Menu/fromTheWok.js
@@ -5,10 +5,219 @@ const FromTheWok = () => {
       <h2 className="heading-title text-center underline-60">From The Wok</h2>
       <div className="row">
         <div className="col-12 col-md-6 menu-list__description">
-          Menu List #1
+          <h2 className="menu-item__title">14. Thai Cashew Nuts</h2>
+          <p className="menu-item__price lunch-dinner">
+            <span class="lunch">Lunch</span> $9.95 Chicken, Pork, or Tofu |
+            $12.95 Beef or Shrimp | $13.95 Seafood
+          </p>
+          <p className="menu-item__price lunch-dinner">
+            <span class="dinner">Dinner</span> $11.95 Chicken, Pork, or Tofu |
+            $14.95 Beef or Shrimp | $18.95 Seafood
+          </p>
+          <p className="menu-item__description">
+            Meat or TOFU stir-fried with chili oil and roasted cashew nuts,
+            onion, broccoli, fresh carrot and bell peppers.
+          </p>
+
+          <figure class="image">
+            <img
+              class="image-75 image-rounded centered-and-cropped"
+              src="/images/img-food-thai-cashew-nuts-chicken.jpg"
+              alt="Mali Thai Bistro - Thai Cashew Nuts Chicken"
+            />
+          </figure>
+          <p className="menu-item__caption">Thai Cashew Nuts Chicken</p>
+
+          <h2 className="menu-item__title">15. Garlic and Pepper</h2>
+          <p className="menu-item__price lunch-dinner">
+            <span class="lunch">Lunch</span> $9.95 Chicken, Pork, or Tofu |
+            $12.95 Beef or Shrimp | $13.95 Seafood
+          </p>
+          <p className="menu-item__price lunch-dinner">
+            <span class="dinner">Dinner</span> $11.95 Chicken, Pork, or Tofu |
+            $14.95 Beef or Shrimp | $18.95 Seafood
+          </p>
+          <p className="menu-item__description">
+            A delicious stir-fried with garlic and ground black pepper,
+            presented in bed of cabbages.
+          </p>
+
+          <h2 className="menu-item__title">16. Vegetable Fried Rice</h2>
+          <p className="menu-item__price lunch-dinner">
+            <span class="lunch">Lunch</span> $9.95 Chicken, Pork, or Tofu |
+            $12.95 Beef or Shrimp | $13.95 Seafood
+          </p>
+          <p className="menu-item__price lunch-dinner">
+            <span class="dinner">Dinner</span> $11.95 Chicken, Pork, or Tofu |
+            $14.95 Beef or Shrimp | $18.95 Seafood
+          </p>
+          <p className="menu-item__description">
+            Stir-fried rice with egg, and mixed vegetables.
+          </p>
+
+          <h2 className="menu-item__title">17. Thai Fried Rice</h2>
+          <p className="menu-item__price lunch-dinner">
+            <span class="lunch">Lunch</span> $9.95 Chicken, Pork, or Tofu |
+            $12.95 Beef or Shrimp | $13.95 Seafood
+          </p>
+          <p className="menu-item__price lunch-dinner">
+            <span class="dinner">Dinner</span> $11.95 Chicken, Pork, or Tofu |
+            $14.95 Beef or Shrimp | $18.95 Seafood
+          </p>
+          <p className="menu-item__description">
+            Stir-fried rice with egg, onion, spring onions and your choice of
+            meat.
+          </p>
+
+          <h2 className="menu-item__title">18. Basil Fried Rice</h2>
+          <p className="menu-item__price lunch-dinner">
+            <span class="lunch">Lunch</span> $9.95 Chicken, Pork, or Tofu |
+            $12.95 Beef or Shrimp | $13.95 Seafood
+          </p>
+          <p className="menu-item__price lunch-dinner">
+            <span class="dinner">Dinner</span> $13.95 Chicken, Pork, or Tofu |
+            $14.95 Beef or Shrimp | $18.95 Seafood
+          </p>
+          <p className="menu-item__description">
+            Stir-fried rice with egg, onion, spring onions, basil and your
+            choice of meats.
+          </p>
+
+          <h2 className="menu-item__title">19. Crab Fried Rice</h2>
+          <p className="menu-item__price lunch-dinner">
+            <span class="lunch">Lunch</span> $9.95 Chicken, Pork, or Tofu |
+            $12.95 Beef or Shrimp | $13.95 Seafood
+          </p>
+          <p className="menu-item__price lunch-dinner">
+            <span class="lunch">Lunch</span> $13.95 |{" "}
+            <span class="dinner">Dinner</span>
+            $15.95
+          </p>
+          <p className="menu-item__description">
+            Stir-fried rice with crab meat, egg, onion and spring onions.{" "}
+          </p>
+
+          <figure class="image">
+            <img
+              class="image-75 image-rounded centered-and-cropped"
+              src="/images/img-food-crab-fried-rice.jpg"
+              alt="Mali Thai Bistro - Crab Fried Rice"
+            />
+          </figure>
+          <p className="menu-item__caption">Crab Fried Rice</p>
+          <h2 className="menu-item__title">20. Yellow Curry Fried Rice</h2>
+          <p className="menu-item__price lunch-dinner">
+            <span class="lunch">Lunch</span> $9.95 Chicken, Pork, or Tofu |
+            $12.95 Beef or Shrimp | $13.95 Seafood
+          </p>
+          <p className="menu-item__price lunch-dinner">
+            <span class="dinner">Dinner</span> $13.95 Chicken, Pork, or Tofu |
+            $14.95 Beef or Shrimp | $18.95 Seafood
+          </p>
+          <p className="menu-item__description">
+            Traditional fried rice with onions, cabbage, napa and scallions and
+            the addition of a wonderful yellow curry flavor.
+          </p>
         </div>
         <div className="col-12 col-md-6 menu-list__description">
-          Menu List #2
+          <h2 className="menu-item__title">21. Pineapple Fried Rice</h2>
+          <p className="menu-item__price lunch-dinner">
+            <span class="lunch">Lunch</span> $12.95 (Seafood $13.95) |{" "}
+            <span class="dinner">Dinner</span>
+            $14.95 (Seafood $18.95)
+          </p>
+          <p className="menu-item__description">
+            Fried rice with shrimp, egg and pineapple combined with cashew nuts
+            and raisins.{" "}
+          </p>
+
+          <figure class="image">
+            <img
+              class="image-75 image-rounded centered-and-cropped"
+              src="/images/img-food-pineapple-fried-rice-shrimp.jpg"
+              alt="Mali Thai Bistro - Pineapple Fried Rice Shrimp"
+            />
+          </figure>
+          <p className="menu-item__caption">Pineapple Fried Rice with Shrimp</p>
+
+          <h2 className="menu-item__title">29. Green Bean Chicken</h2>
+          <p className="menu-item__price lunch-dinner">
+            <span class="lunch">Lunch</span> $9.95 Chicken, Pork, or Tofu |
+            $12.95 Beef or Shrimp | $13.95 Seafood
+          </p>
+          <p className="menu-item__price lunch-dinner">
+            <span class="dinner">Dinner</span> $11.95 Chicken, Pork, or Tofu |
+            $14.95 Beef or Shrimp | $18.95 Seafood
+          </p>
+          <p className="menu-item__description">
+            Sautéed with green beans in a spicy curry sauce.
+          </p>
+
+          <h2 className="menu-item__title">39. Seafood Fried Rice</h2>
+          <p className="menu-item__price lunch-dinner">
+            <span class="lunch">Lunch</span>
+            $13.95 | <span class="dinner">Dinner</span> $18.95
+          </p>
+          <p className="menu-item__description">
+            Our signature fried rice made with jumbo shrimp, scallops, imatation
+            crab meat and squid.
+          </p>
+
+          <h2 className="menu-item__title">40. Crispy Garlic Shrimp</h2>
+          <p className="menu-item__price lunch-dinner">
+            <span class="lunch">Lunch</span>/<span class="dinner">Dinner</span>
+            $18.95
+          </p>
+          <p className="menu-item__description">
+            Jumbo shrimp flash fried and then sautéed in our delicious garlic
+            sauce. Served over a bed of steamed vegetables.
+          </p>
+
+          <figure class="image">
+            <img
+              class="image-50 image-rounded centered-and-cropped"
+              src="/images/img-food-veggieandshrimps.jpg"
+              alt="Mali Thai Bistro - Crispy Garlic Shrimp"
+            />
+          </figure>
+          <p className="menu-item__caption">Crispy Garlic Shrimp</p>
+          <h2 className="menu-item__title">41. Pad Pak Rom</h2>
+          <p className="menu-item__price lunch-dinner">
+            <span class="lunch">Lunch</span> $9.95 Chicken, Pork, or Tofu |
+            $12.95 Beef or Shrimp | $13.95 Seafood
+          </p>
+          <p className="menu-item__price lunch-dinner">
+            <span class="dinner">Dinner</span> $11.95 Chicken, Pork, or Tofu |
+            $14.95 Beef or Shrimp | $18.95 Seafood
+          </p>
+          <p className="menu-item__description">
+            Stir-fried mixed vegetable with homemade garlic brown sauce.
+          </p>
+
+          <h2 className="menu-item__title">42. Pad Khing</h2>
+          <p className="menu-item__price lunch-dinner">
+            <span class="lunch">Lunch</span> $9.95 Chicken, Pork, or Tofu |
+            $12.95 Beef or Shrimp | $13.95 Seafood
+          </p>
+          <p className="menu-item__price lunch-dinner">
+            <span class="dinner">Dinner</span> $11.95 Chicken, Pork, or Tofu |
+            $14.95 Beef or Shrimp | $18.95 Seafood
+          </p>
+          <p className="menu-item__description">
+            Meat or TOFU Stir fried with fresh ginger root, chinese mushroom,
+            onion, scallion and bell pepper.
+          </p>
+
+          <h2 className="menu-item__title">43. Pad Pong Ka Ree Rom</h2>
+          <p className="menu-item__price lunch-dinner">
+            <span class="lunch">Lunch</span>
+            $13.95 | <span class="dinner">Dinner</span> $18.95
+          </p>
+          <p className="menu-item__description">
+            Combination of seafood (Jumbo Shrimp, Scallop, Squid, Crab meat)
+            stir-fried with yellow curry powder, egg, onion, celery, bell
+            pepper, spring onion and napa.
+          </p>
         </div>
       </div>
     </div>
