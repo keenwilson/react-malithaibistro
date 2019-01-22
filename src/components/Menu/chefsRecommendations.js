4dafdface1636ef921fdb18928fa5231536b725e
Keen Wilson <40321520+keenwilson@users.noreply.github.com>
2019-01-22 11:55:07 -0600

    Add Chef's recommendations

--- src/components/Menu/chefsRecommendations.js
+++ src/components/Menu/chefsRecommendations.js
@@ -8,10 +8,18 @@ const ChefsRecommendations = () => {
       </h2>
       <div className="row">
         <div className="col-12 col-md-6 menu-list__description">
-          Menu List #1
+          <h2 className="menu-item__title">Fresh Shrimp Roll</h2>
+          <p className="menu-item__price ">$8.95</p>
+
+          <h2 className="menu-item__title">Lemongrass Beef</h2>
+          <p className="menu-item__price">$9.95</p>
         </div>
         <div className="col-12 col-md-6 menu-list__description">
-          Menu List #2
+          <h2 className="menu-item__title">Pot Sticker (steam/pan fry)</h2>
+          <p className="menu-item__price">$7.95</p>
+
+          <h2 className="menu-item__title">Rad Na Taley (House Favorite)</h2>
+          <p className="menu-item__price">$18.95</p>
         </div>
       </div>
     </div>
