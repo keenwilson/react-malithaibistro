d2f443d552d940167f9fb53fbfcd852f2e159789
Keen Wilson <40321520+keenwilson@users.noreply.github.com>
2019-01-22 19:13:00 -0600

    Updates

---
+++ precache-manifest.3d5e557bee979bf6391033b96f00f97a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "99c67f905feddfa16a37",
+    "url": "/react-malithaibistro/static/css/main.3756a49a.chunk.css"
+  },
+  {
+    "revision": "99c67f905feddfa16a37",
+    "url": "/react-malithaibistro/static/js/main.99c67f90.chunk.js"
+  },
+  {
+    "revision": "06408a80ece2949f340e",
+    "url": "/react-malithaibistro/static/js/1.06408a80.chunk.js"
+  },
+  {
+    "revision": "05dd23c73b3c0dc4d13e",
+    "url": "/react-malithaibistro/static/js/runtime~main.05dd23c7.js"
+  },
+  {
+    "revision": "291712a71cc5bf76c4973ec0a39d6dd0",
+    "url": "/react-malithaibistro/index.html"
+  }
+];
